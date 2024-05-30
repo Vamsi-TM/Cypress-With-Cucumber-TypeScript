508ae9cebc243c27350a1e68d1811bc7cb36d6f3
Vamsi-TM <vamsi@timeandmoneyltd.co.uk>
2024-05-30 17:19:43 +0100

    Added cucumber parallel plugin and updated the api tests for better readability

--- cypress/e2e/features/api/api_testing.feature
+++ cypress/e2e/features/api/api_testing.feature
@@ -1,8 +1,16 @@
 @api
 Feature: As a user, I want to demo how Cypress can be used for tesing APIs
-@test 
-Scenario: Test to demo Create method
-Given I create users the response should be "201"
+using create and update methods.
 
-Scenario: Test to demo the Update method
- Given I update users the response should be "200"
+  @createMethod
+  Scenario: Create new users successfully
+    Given I create a new user with name "John Smith" and job title "Developer"
+    Then the status code should be "201"
+    And the response status text should be "Created"
+
+  @updateMethod
+  Scenario: Update the users created
+    Given I update the job title for "John Smith" to "Sr Developer"
+    Then the status code should be "200"
+    And the response status text should be "OK"
+    And the new job title should be "Sr Developer"
--- cypress/e2e/steps/stepDefinitions/testAPIs.ts
+++ cypress/e2e/steps/stepDefinitions/testAPIs.ts
@@ -2,32 +2,68 @@ import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
 
 const DUMMY_API = 'https://reqres.in/api/users';
 
-Given('I create users the response should be {string}', (stg: string) => {
-  cy.request({
-    method: 'POST',
-    url: DUMMY_API,
-    body: {
-      name: 'Joe Blobbs',
-      job: 'Automation Tester'
-    }
-  }).then((response) => {
-    expect(response.status).to.eq(Number(stg));
-    expect(response.body.name).to.eq('Joe Blobbs');
-    expect(response.statusText).to.eq('Created');
-  });
+Given(
+  'I create a new user with name {string} and job title {string}',
+  (newUserName: string, jobTitle: string) => {
+    cy.request({
+      method: 'POST',
+      url: DUMMY_API,
+      body: {
+        name: newUserName,
+        job: jobTitle
+      }
+    })
+      .then((response) => {
+        expect(response.status).to.eq(201);
+      })
+      .as('methodCreateResponse');
+  }
+);
+
+Then('the response status text should be {string}', (stg: string) => {
+  if (stg == 'Created') {
+    cy.get('@methodCreateResponse').should((response) => {
+      expect(response).to.have.property('statusText').to.eq(stg);
+    });
+  } else if (stg == 'OK') {
+    cy.get('@methodUpdateResponse').should((response) => {
+      expect(response).to.have.property('statusText').to.eq(stg);
+    });
+  }
 });
 
-Given('I update users the response should be {string}', (stg: string) => {
-  cy.request({
-    method: 'PUT',
-    url: DUMMY_API + '/2',
-    body: {
-      name: 'Joe Blobbs',
-      job: 'Developer'
-    }
-  }).then((response) => {
-    expect(response.status).to.eq(Number(stg));
-    expect(response.body.job).to.eq('Developer');
-    expect(response.statusText).to.eq('OK');
+Then('the status code should be {string}', (statusCode: string) => {
+  if (statusCode == '201') {
+    cy.get('@methodCreateResponse').should((response) => {
+      expect(response).to.have.property('status').to.eq(Number(statusCode));
+    });
+  } else if (statusCode == '200') {
+    cy.get('@methodUpdateResponse').should((response) => {
+      expect(response).to.have.property('status').to.eq(Number(statusCode));
+    });
+  }
+});
+
+Given(
+  'I update the job title for {string} to {string}',
+  (newUserName: string, jobTitle: string) => {
+    cy.request({
+      method: 'PUT',
+      url: DUMMY_API + '/2',
+      body: {
+        name: newUserName,
+        job: jobTitle
+      }
+    })
+      .then((response) => {
+        expect(response.status).to.eq(200);
+      })
+      .as('methodUpdateResponse');
+  }
+);
+
+Then('the new job title should be {string}', (newJobTitle: string) => {
+  cy.get('@methodUpdateResponse').should((response) => {
+    expect(response).to.have.property('body').to.include({'job':newJobTitle})
   });
 });
